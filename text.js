25e2a85539a3b5115a5eb203b892d151b1ac6888
sanjsndas <jaintipu65658@gmail.com>
2025-08-01 03:15:56 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-833-200-4572';
+window.defaultText=' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!';
+window.text ={
+	'xhamster.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'perfectgirls.net':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'gotporn.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'anysex.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'sex.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'bravotube.net':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'mylust.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'manporn.xxx':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'anybunny.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'txxx.com':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!',
+	'findbestsolution.xyz':' Your iPhone has been locked due to detected illegal Child Pornography. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-888-222-1111 To Unlock It!'
+};
